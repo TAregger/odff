390d54187c45566bc025fa8067a16c72994ad9a8
Thomas Aregger <thomas.aregger@gmail.com>
2022-02-05 23:30:01 +0100

    Use BufferedWriter instead of BufferedOutputStream

--- src/main/java/io/aregger/odff/service/TracefileWriter.java
+++ src/main/java/io/aregger/odff/service/TracefileWriter.java
@@ -3,18 +3,17 @@ package io.aregger.odff.service;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.UncheckedIOException;
 import java.lang.invoke.MethodHandles;
-import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.text.NumberFormat;
 import java.util.function.Consumer;
 
+import static java.nio.file.StandardOpenOption.CREATE_NEW;
 import static java.util.Objects.requireNonNull;
 
 public class TracefileWriter {
@@ -29,9 +28,9 @@ public class TracefileWriter {
     }
 
     void writeFile(String tracefileName, DatabaseFileFetcher fetcher) throws IOException {
-        File file = FileUtils.createFile(this.currentDir, tracefileName);
-        try (BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(file))) {
-            fetcher.fetchTracefile(writeLine(outputStream));
+        File file = this.currentDir.resolve(tracefileName).toFile();
+        try (BufferedWriter bufferedWriter = Files.newBufferedWriter(file.toPath(), CREATE_NEW)) {
+            fetcher.fetchTracefile(writeLine(bufferedWriter));
         } finally {
             if (file.length() == 0) {
                 // If no rows are returned by the database, e.g. because a non-existent tracefile was specified by the user,
@@ -45,7 +44,7 @@ public class TracefileWriter {
         }
     }
 
-    private static Consumer<String> writeLine(OutputStream outputStream) {
+    private static Consumer<String> writeLine(BufferedWriter bufferedWriter) {
         return line -> {
             try {
                 // gv$diag_trace_file_contents.payload can be null.
@@ -53,7 +52,7 @@ public class TracefileWriter {
                     return;
                 }
                 // Lines in gv$diag_trace_file_contents.payload and v$diag_alert_ext.message_text already contain a trailing newline character.
-                outputStream.write(line.getBytes(StandardCharsets.UTF_8));
+                bufferedWriter.write(line);
             } catch (IOException e) {
                 throw new UncheckedIOException(e);
             }
